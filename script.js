2255ebc526d011c23e77f40e524c4deaba198558
Chiagoziem Ede <elotachukwu@gmail.com>
2020-10-02 12:44:40 +0100

    Adding CSS styles and JavaScript functions

---
+++ script.js
@@ -0,0 +1,70 @@
+const button1 = document.querySelector('.start');
+const page1 = document.querySelector('.welcome');
+const page2 = document.querySelector('.heading');
+const page3 = document.querySelector('.response');
+const submitButton = document.querySelector('[type="submit"]');
+const resetButton = document.querySelector('.reset');
+
+button1.addEventListener('click',function(){
+    page2.classList.remove('hidden');
+    page1.classList.add('hidden');
+})
+
+submitButton.addEventListener('click',function(){
+    page3.classList.remove('hidden');
+    page2.classList.add('hidden');
+})
+
+resetButton.addEventListener('click',function(){
+    page1.classList.remove('hidden');
+    page3.classList.add('hidden');
+})
+
+function calculateAge(){
+    var name = document.getElementById("nameId").value;
+    var age = document.getElementById("ageId").value;
+
+    let name1 = document.getElementById("nameResId");
+    name1.textContent = name;
+
+    let age2 = document.getElementById("ageResId");
+    age2.textContent = age;
+
+    let ageInDays;
+    ageInDays = age * 365;
+    ageInDays1 = document.getElementById("daysResId");
+    ageInDays1.textContent = ageInDays
+
+    if(name==="" && age===""){
+        alert("Fill in all fields!");
+        return;
+    } else if(name===""){
+        alert("Fill in your name.");
+        return;
+    } else if(age===""){
+        alert("Fill in your age");
+        return;
+    } 
+
+// }
+// var nowDate = new Date(new Date().setHours(0, 0, 0, 0));
+// console.log(nowDate);
+//     // Example date of birth.
+//     var dobDate = new Date();
+//     console.log(dobDate);
+
+//     var years = nowDate.getFullYear() - dobDate.getFullYear();
+//     var months = nowDate.getMonth() - dobDate.getMonth();
+//     var days = nowDate.getDate() - dobDate.getDate();
+
+//     function calculateAge(dateString) {
+//         var today = new Date();
+//         var birthDate = new Date(dateString);
+//         var age = today.getFullYear() - birthDate.getFullYear();
+//         var m = today.getMonth() - birthDate.getMonth();
+//         if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//             age--;
+//         }
+//         return age;
+//     }
+}
