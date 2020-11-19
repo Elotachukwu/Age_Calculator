d5884462cc6da26a1613979496d5259fcda201fc
Chiagoziem Ede <elotachukwu@gmail.com>
2020-11-19 14:00:50 +0100

    Changing the indentation to 2-spaces

--- script.js
+++ script.js
@@ -6,45 +6,45 @@ const submitButton = document.querySelector('[type="submit"]');
 const resetButton = document.querySelector('.reset');
 
 button1.addEventListener('click',function(){
-    page2.classList.remove('hidden');
-    page1.classList.add('hidden');
+  page2.classList.remove('hidden');
+  page1.classList.add('hidden');
 })
 
 submitButton.addEventListener('click',function(){
-    page3.classList.remove('hidden');
-    page2.classList.add('hidden');
+  page3.classList.remove('hidden');
+  page2.classList.add('hidden');
 })
 
 resetButton.addEventListener('click',function(){
-    page1.classList.remove('hidden');
-    page3.classList.add('hidden');
+  page1.classList.remove('hidden');
+  page3.classList.add('hidden');
 })
 
 function calculateAge(){
-    var name = document.getElementById("nameId").value;
-    var age = document.getElementById("ageId").value;
+  var name = document.getElementById("nameId").value;
+  var age = document.getElementById("ageId").value;
 
-    let name1 = document.getElementById("nameResId");
-    name1.textContent = name;
+  let name1 = document.getElementById("nameResId");
+  name1.textContent = name;
 
-    let age2 = document.getElementById("ageResId");
-    age2.textContent = age;
+  let age2 = document.getElementById("ageResId");
+  age2.textContent = age;
 
-    let ageInDays;
-    ageInDays = age * 365;
-    ageInDays1 = document.getElementById("daysResId");
-    ageInDays1.textContent = ageInDays
+  let ageInDays;
+  ageInDays = age * 365;
+  ageInDays1 = document.getElementById("daysResId");
+  ageInDays1.textContent = ageInDays
 
-    if(name==="" && age===""){
-        alert("Fill in all fields!");
-        return;
-    } else if(name===""){
-        alert("Fill in your name.");
-        return;
-    } else if(age===""){
-        alert("Fill in your age");
-        return;
-    } 
+  if(name==="" && age===""){
+    alert("Fill in all fields!");
+    return;
+  } else if(name===""){
+    alert("Fill in your name.");
+    return;
+  } else if(age===""){
+    alert("Fill in your age");
+    return;
+  } 
 }
 
 
@@ -52,15 +52,15 @@ var body = document.querySelector("body");
 var isBlue = false;
 
 setInterval(function(){
-    if (isBlue){
-        body.style.background = "yellow";
-    } else {
-        body.style.background = "#3498db"
-    }
-    isBlue = !isBlue;
+  if (isBlue){
+    body.style.background = "yellow";
+  } else {
+    body.style.background = "#3498db"
+  }
+  isBlue = !isBlue;
 }, 1000)
 
 var button = document.querySelector("button");
 button.addEventListener("click", function(){
-    console.log("Someone clicked the button!");
+  console.log("Someone clicked the button!");
 });
